a20be03c242034f9c5cbebbcf5352a5b43f31270
Felipe Montenegro Ribeiro <felipe.mribeiro@al.infnet.edu.br>
2022-03-25 21:30:27 -0300

    create state control and input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React, { useState } from 'react'
 
 
 function App() {
+  //valor do estado setado inicialmente como vazio
+  const [product, setProduct] = useState('')
+
+  function handleSearch() {
+
+  }
+
   return (
-<input name="usuario" id="usuario" className="userInput" placeholder="" />
+    <>
+      {/*função para atualizar o valor do estado através do input */}
+      <input className="userInput" placeholder="Product" value={product} onChange={e => setProduct(e.target.value)} />
+      <button type="button" onClick={handleSearch} >Search</button>
+    </>
   );
 }
 
